b4c6cff33c5e95a5e6d442d1945532326b62386e
VictorSolique <victordesouzasolique@gmail.com>
2023-09-14 16:51:03 -0400

    commit lista 6

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.render('index', { message: 'Super Mercado Atacadinho - As melhores ofertas!' });
 });
 
+app.get('/produtos', (req, res) => {
+  res.render('produto', { message: 'Farinha de Trigo Tipo 1 Pacote 1kg - Coamo' });
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
